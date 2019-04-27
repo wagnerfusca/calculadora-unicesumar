c15bd7a423a25dbc5255e017f78d781b3ce1b3ab
fusca <wagnervoltz@gmail.com>
2019-04-27 11:53:14 -0300

    inclusao de banco de dados - parte 1

---
+++ src/main/java/br/edu/unicesumar/dao/ProdutoDao.java
@@ -0,0 +1,23 @@
+package br.edu.unicesumar.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import br.edu.unicesumar.model.Produto;
+
+public class ProdutoDao extends DAO {
+
+	public void save(Produto produto) {
+		EntityManager manager = getEntityManager();
+
+		manager.getTransaction().begin();
+		manager.persist(produto);
+		manager.getTransaction().commit();
+	}
+
+	public List<Produto> list() {
+		EntityManager manager = getEntityManager();
+
+		return manager.createQuery("from Produto").getResultList();
+	}
+
+}
