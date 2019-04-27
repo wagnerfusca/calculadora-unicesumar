1fa6be67f5e52048914098202c7ec94a37074d48
fusca <wagnervoltz@gmail.com>
2019-04-27 14:32:37 -0300

    finalizacao da configuracao da base de dados

--- src/main/java/br/edu/unicesumar/model/Produto.java
+++ src/main/java/br/edu/unicesumar/model/Produto.java
@@ -1,8 +1,22 @@
 package br.edu.unicesumar.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column
 	private String nome;
+	
+	@Column
 	private int quantidade;
 
 	public String getNome() {
--- src/main/java/br/edu/unicesumar/rest/ProdutoService.java
+++ src/main/java/br/edu/unicesumar/rest/ProdutoService.java
@@ -1,5 +1,7 @@
 package br.edu.unicesumar.rest;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,11 +9,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import br.edu.unicesumar.dao.ProdutoDao;
 import br.edu.unicesumar.model.Produto;
 
 @Path("/json/produto")
 public class ProdutoService {
 
+	private ProdutoDao dao = new ProdutoDao();
+	
 	@GET
 	@Path("/get")
 	@Produces("application/json")
@@ -20,7 +25,7 @@ public class ProdutoService {
 		Produto produto = new Produto();
 		produto.setNome("Moto X - Android");
 		produto.setQuantidade(10);
-
+	
 		return produto;
 
 	}
@@ -30,8 +35,22 @@ public class ProdutoService {
 	@Consumes("application/json")
 	public Response criarProdutoJSON(Produto produto) {
 
+		dao.save(produto);
+		
 		String result = "produto Criado : " + produto.toString();
 		return Response.status(201).entity(result).build();
+
+	}
+	
+	
+	@GET
+	@Path("/listas")
+	@Produces("application/json")
+	public Response listar() {
+
+		List<Produto> produtos = dao.list();
+		String resultado = "quantidade de produtos: "+ produtos.size();
+		return Response.status(201).entity(resultado).build();
 
 	}
 
