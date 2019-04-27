98fbfe5551014f69e5a5221baa9e900b88aa2aea
fusca <wagnervoltz@gmail.com>
2019-04-27 11:31:42 -0300

    novo servico

---
+++ src/main/java/br/edu/unicesumar/model/Produto.java
@@ -0,0 +1,24 @@
+package br.edu.unicesumar.model;
+
+public class Produto {
+
+	private String nome;
+	private int quantidade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
---
+++ src/main/java/br/edu/unicesumar/rest/ProdutoService.java
@@ -0,0 +1,38 @@
+package br.edu.unicesumar.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import br.edu.unicesumar.model.Produto;
+
+@Path("/json/produto")
+public class ProdutoService {
+
+	@GET
+	@Path("/get")
+	@Produces("application/json")
+	public Produto getProdutoJSON() {
+
+		Produto produto = new Produto();
+		produto.setNome("Moto X - Android");
+		produto.setQuantidade(10);
+
+		return produto;
+
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes("application/json")
+	public Response criarProdutoJSON(Produto produto) {
+
+		String result = "produto Criado : " + produto.toString();
+		return Response.status(201).entity(result).build();
+
+	}
+
+}
