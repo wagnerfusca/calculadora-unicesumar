14efb3962d7d52b0b42f2ac644242a31847b6709
fusca <wagnervoltz@gmail.com>
2019-04-27 10:13:03 -0300

    versionando o primeiro exemplo de REST

---
+++ Calculadora/src/main/java/br/edu/unicesumar/rest/CalculadoraService.java
@@ -0,0 +1,19 @@
+package br.edu.unicesumar.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/calculadora")
+public class CalculadoraService {
+
+	@GET
+	@Path("/teste/{param1}/{param2}")
+	public Response olaMundo(@PathParam("param1") String mensagem1, @PathParam("param2") String mensagem2) {
+		
+		String texto = "ola: mensagem 1 " + mensagem1 + " - " + mensagem2;
+		
+		return Response.status(200).entity(texto).build();
+	}
+}
